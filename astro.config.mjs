9cb3d369ea40e4fb9626a23cc0784e3e3a79af98
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2026-05-30 17:54:07 -0700

    add project config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://photos.aksads.tech",
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [sitemap()],
+  prefetch: true,
+  vite: {
+    ssr: {
+      noExternal: ["smartypants"],
+    },
+  },
+});
